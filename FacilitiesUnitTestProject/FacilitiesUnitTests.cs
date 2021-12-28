12da8ec346215b6c1910d4bad8f13e375d19b3b6
alex20317823073857 <93069649+alex20317823073857@users.noreply.github.com>
2021-12-28 13:46:16 +0500

    Программа из условия

---
+++ FacilitiesUnitTestProject/FacilitiesUnitTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Facilities;
+
+namespace FacilitiesUnitTestProject
+{
+    [TestClass]
+    public class FacilitiesUnitTests
+    {
+        [TestMethod]
+        public void ClientTest()
+        {
+            var client = new Client();
+
+            Assert.AreEqual(34, client.DoCalls());
+        }
+    }
+}
