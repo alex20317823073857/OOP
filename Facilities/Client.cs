04257ba740849cd65909492d14d256a327bc09b8
alex20317823073857 <93069649+alex20317823073857@users.noreply.github.com>
2021-12-28 13:47:34 +0500

    Результат выполнения задания

--- Facilities/Client.cs
+++ Facilities/Client.cs
@@ -8,70 +8,25 @@ namespace Facilities
 {
     public class Client
     {
-        Support support;
+        Facade facade = new Facade();
 
         public int DoCalls()
         {
+            
             int counter = 0;
 
-            support = new Support();
-            support.Engineer.SubmitNetworkRequest();
-            support.Electrician.SubmitNetworkRequest();
-            support.Technician.SubmitNetworkRequest();
-
-            bool electicianDone = false;
-            bool engineerDone = false;
-            bool technicianDone = false;
-
-            while (!technicianDone)
+            while (!CallSupport())
             {
-                counter++;
-                if (!engineerDone)
-                    engineerDone = CallEngineer();
-                else if (engineerDone && !electicianDone)
-                    electicianDone = CallElectrician();
-                else if (electicianDone && !technicianDone)
-                    technicianDone = CallTechnician();
+                CallSupport();
             }
 
             return counter;
         }
 
-
-        bool CallElectrician()
+        bool CallSupport()
         {
-            // Если электрик свою работу выполнил, 
-            // перенаправим запрос технику
-            if (support.Electrician.CheckOnStatus())
-            {
-                support.Technician.SubmitNetworkRequest();
-                return true;
-            }
-
-            return false;
-        }
-
-        bool CallEngineer()
-        {
-            // Если инженер свою работу выполнил, 
-            // перенаправим запрос электрику
-            if (support.Engineer.CheckOnStatus())
-            {
-                support.Electrician.SubmitNetworkRequest();
-                return true;
-            }
-
-            return false;
-        }
-
-        bool CallTechnician()
-        {
-            // Если техник свою работу выполнил, 
-            // то запрос обслужен до конца
-            if (support.Technician.CheckOnStatus())
-                return true;
-
-            return false;
+            facade.DoWorkCalls();
+            return facade.CurrentStateOfWork();
         }
     }
 }
---
+++ Facilities/Facade.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facilities
+{
+    public class Facade
+    {
+        public Support support = new Support();
+
+        public int DoWorkCalls()
+        {
+            int counter = 0;
+            
+            support.Engineer.SubmitNetworkRequest();
+            support.Electrician.SubmitNetworkRequest();
+            support.Technician.SubmitNetworkRequest();
+
+            bool electicianDone = false;
+            bool engineerDone = false;
+            bool technicianDone = false;
+
+            while (!technicianDone)
+            {
+                counter++;
+                if (!engineerDone)
+                    engineerDone = CallEngineer();
+                else if (engineerDone && !electicianDone)
+                    electicianDone = CallElectrician();
+                else if (electicianDone && !technicianDone)
+                    technicianDone = CallTechnician();
+            }
+
+            return counter;
+        }
+
+
+        bool CallElectrician()
+        {
+            // Если электрик свою работу выполнил, 
+            // перенаправим запрос технику
+            if (support.Electrician.CheckOnStatus())
+            {
+                support.Technician.SubmitNetworkRequest();
+                return true;
+            }
+
+            return false;
+        }
+
+        bool CallEngineer()
+        {
+            // Если инженер свою работу выполнил, 
+            // перенаправим запрос электрику
+            if (support.Engineer.CheckOnStatus())
+            {
+                support.Electrician.SubmitNetworkRequest();
+                return true;
+            }
+
+            return false;
+        }
+
+        bool CallTechnician()
+        {
+            // Если техник свою работу выполнил, 
+            // то запрос обслужен до конца
+            if (support.Technician.CheckOnStatus())
+                return true;
+
+            return false;
+        }
+
+        public bool CurrentStateOfWork()
+        {
+            DoWorkCalls();
+            return CallTechnician();
+        }
+    }
+}
